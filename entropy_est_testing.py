3e878a5f16c9a8cba4930078ad9ae749ff13d5e1
ConstantijnvdP <constantijnvdp@gmail.com>
2019-06-10 20:48:59 -0400

    Testing numeric integration methods

--- entropy_est_testing.py
+++ entropy_est_testing.py
@@ -3,12 +3,17 @@ import timeit
 import numpy as np
 import matplotlib.pyplot as plt
 
+from scipy.stats import beta
+from scipy.integrate import quad
+
+from numba import complex128, float64, jit
+
 import entropy_estimators as ee
 import bandit as Ban #Insert 7 Deadly Sins anime reference
 
 
-timing_runs = 1000   # Number of runs to determine computation time
-acc_runs = 10000      # Number of runs to determine accuracy
+timing_runs = 2   # Number of runs to determine computation time
+acc_runs = 2      # Number of runs to determine accuracy
 
 
 ## Testing Sampling
@@ -46,7 +51,6 @@ J_MSE = np.mean( (J_est_list-ent_target)**2 )
 print("Jimmy's Mean:", J_est_mean)
 print("Jimmy's MSE:", J_MSE)
 '''
-
 # Nearest Neighbor Entropy Estimation
 def nn_entropy(a1,a2,b1,b2, N_disc=50, N_sample=10000):
     Rhomax_vec = np.vectorize(lambda p: Ban.Rhomax(p, a1, a2, b1, b2))
@@ -74,53 +78,59 @@ def entropy_est2(a1,a2,b1,b2, N_disc=50, N_sample=10000):
     sample_lst = np.append( sample_lst, np.random.choice(-np.log2(Rhomax_disc), p=Rhomax_disc_nrm) )
     return np.mean(sample_lst)
 
+# Playing around with numeric integration
+#@jit(complex128(float64, float64, float64,float64,float64), nopython=True, cache=True)
+def rhomax_integrand(x,a1,a2,b1,b2):
+    pdf1 = beta.pdf(x,a1,b1)
+    pdf2 = beta.pdf(x,a2,b2)
+    cdf1 = beta.cdf(x,a1,b1)
+    cdf2 = beta.cdf(x,a2,b2)
+    
+    rho = pdf1 * cdf2 + pdf2 * cdf1
+
+    integrand = -rho * np.log( rho )
+    return integrand
+def entropy_int2(integrand, arg):
+    integral = quad(integrand, 0, 1, args=(arg))[0]
+    return integral
+
+def entropy_int3(a1,a2,b1,b2):
+    def integrand(x):
+        pdf1 = beta.pdf(x,a1,b1)
+        pdf2 = beta.pdf(x,a2,b2)
+        cdf1 = beta.cdf(x,a1,b1)
+        cdf2 = beta.cdf(x,a2,b2)
+        rho = pdf1 * cdf2 + pdf2 * cdf1
+        return -rho * np.log(rho)
+    integral = quad(integrand, 0, 1)[0]
+    return integral
+
 
 ##Timing
-# Jimmy Estimation
-t_start = time.time()
-for i in range(timing_runs):
-    Ban.Entropy_est(2,5,5,2)
-t_fin = time.time()
-print("Jimmy's Estimator:", t_fin-t_start)
-
-# Jimmy Estimation 2
-t_start = time.time()
-for i in range(timing_runs):
-    entropy_est2(2,5,5,2)
-t_fin = time.time()
-print("Jimmy 2 Estimator:", t_fin-t_start)
-
-# NPEET
-t_start = time.time()
-for i in range(timing_runs):
-    nn_entropy(2,5,5,2)
-t_fin = time.time()
-print("NPEET Estimator:", t_fin-t_start)
-
-# Full Integration
-t_start = time.time()
-for i in range(timing_runs):
-    Ban.Entropy_int(2,5,5,2)
-t_fin = time.time()
-print("Full Integral:", t_fin-t_start)
-
+def time_test(fnc, args, trials=timing_runs):
+    t_start = time.time()
+    for i in range(trials):
+        fnc(*args)
+    t_fin = time.time()
+    return t_fin-t_start
+print("Integral 1:", time_test(Ban.Entropy_int, (2,5,5,2) ) )
+print("Integral 2:", time_test(entropy_int2, (rhomax_integrand,(2,5,5,2)) ) )
+print("Integral 3:", time_test(entropy_int3, (2,5,5,2) ) )
 
 ## Accuracy
-'''
 ent_target = Ban.Entropy_int(2,5,5,2)
-##print("Target Entropy:", ent_target)
-
-ent_est_lst = np.array([])
-for i in range(acc_runs):
-    ent_est_lst = np.append( ent_est_lst, nn_entropy(2,5,5,2) )
-ent_est_mean = np.mean(ent_est_lst)
-MSE = np.mean( (ent_est_lst-ent_target)**2 )
-print("Mean:", ent_est_mean)
-print("MSE:", MSE)
+def acc_test(fnc, args, trials=acc_runs):
+    ent_est_lst = np.array([])
+    for i in range(trials):
+        ent_est_lst = np.append( ent_est_lst, fnc(*args) )
+    ent_est_mean = np.mean(ent_est_lst)
+    MSE = np.mean( (ent_est_lst-ent_target)**2 )
+    return ent_est_mean, MSE
+print("Integral 1:", acc_test(Ban.Entropy_int, (2,5,5,2) ) )
+print("Integral 2:", acc_test(entropy_int2, (rhomax_integrand,(2,5,5,2)) ) )
+print("Integral 3:", acc_test(entropy_int3, (2,5,5,2) ) )
 '''
-
 ## Plotting (but not scheming)
-'''
 plt.hist(x=J_est_list, bins='auto')
 #plt.axvline( x=ent_target, color='r', linewidth=1, linestyle='--' )
 plt.axvline( x=J_est_mean, color='b', linewidth=1, linestyle='--' )
